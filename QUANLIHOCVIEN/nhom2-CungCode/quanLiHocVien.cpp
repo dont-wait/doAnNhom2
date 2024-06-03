d3fc7657f9cc2ae8139051ce540bb465562a75c5
Sang NT <sang-nt>
2024-06-03 08:52:20 +0700

    update case in searchStudent | fix case menu: must upload file to run function

--- QUANLIHOCVIEN/nhom2-CungCode/quanLiHocVien.cpp
+++ QUANLIHOCVIEN/nhom2-CungCode/quanLiHocVien.cpp
@@ -1,338 +1,329 @@
-﻿//
-//
-//#include "prototype-HocVien.h"
-////tim kiem
-//void searchStudent(studentList sl) {
-//	fflush(stdin);
-//	int choice;
-//	studentNode* p;
-//	
-//	printf("1.Tim hoc vien theo ten lop\n");
-//	printf("2.Tim hoc vien theo ma hoc vien\n");
-//	printf("3.Tim hoc vien theo nam sinh\n");
-//	printf("Chon 1..3: ");
-//	scanf("%d", &choice); 
-//	fflush(stdin);
-//	switch (choice)
-//	{
-//	case 1:  {
-//		char tenLop[30];
-//		printf("Nhap ten lop: ");
-//		gets_s(tenLop);
-//		//LAMBDA EXPRESSION
-//		//TÌm kiếm theo Tên lớp
-//		auto searchByTenLop = [] (studentList sl, const char* nameRoom) -> studentNode*{
-//			studentNode* currNode = sl.headNode;
-//			while(currNode!=NULL) {
-//				if(strcmpi(currNode->value.lopHoc.maLop, keyTenMon(nameRoom)) == 0)
-//					return currNode;
-//				currNode = currNode->next;
-//			}
-//			return NULL;
-//		};
-//		p = searchByTenLop(sl, tenLop);
-//		if (p == NULL)
-//			printf("Khong ton tai hoc vien co ten lop: %s\n", tenLop);
-//		else {
-//			p = createSNode(p->value);
-//			showSNode(p);
-//		}
-//		break;
-//	}
-//	case 2: {
-//		char hocVienCoMaHocVien[16];
-//		printf("Nhap ma hoc vien: ");
-//		gets_s(hocVienCoMaHocVien);
-//		//TÌm kiếm theo Mã học viên
-//		auto searchByMaHocVien = [] (studentList sl, char nameClass[]) -> studentNode*{
-//			studentNode* currNode = sl.headNode;
-//			while (currNode != NULL) {
-//				if (stricmp(currNode->value.maHocVien, nameClass) == 0)
-//					return currNode;
-//				currNode = currNode->next;
-//			}
-//			return NULL;
-//		};
-//		
-//		p = searchByMaHocVien(sl, hocVienCoMaHocVien);
-//		if (p == NULL)
-//			printf("Khong ton tai hoc vien co ma: %s\n", hocVienCoMaHocVien);
-//		else {
-//			p = createSNode(p->value);
-//			showSNode(p);
-//		}
-//		break;
-//	}
-//	case 3: {
-//		int year;
-//		printf("Nhap nam sinh: ");
-//		scanf("%d", &year);
-//		//TÌm kiếm theo Năm sinh
-//		auto searchByYOB = [](studentList sl, int year) -> studentNode* {
-//			studentNode* currNode = sl.headNode;
-//			while (currNode != NULL) {
-//				if (currNode->value.birthday.year == year)
-//					return currNode;
-//			currNode = currNode->next;
-//			}
-//			return NULL;
-//		};
-//		
-//		p = searchByYOB(sl, year);
-//		if (p == NULL)
-//			printf("Khong ton tai hoc vien sinh nam: %d\n", year);
-//		else {
-//			p = createSNode(p->value);
-//			showSNode(p);
-//		}
-//		break;
-//	}
-//	default:
-//		break;
-//	}
-//	
-//}
-////=================================================
-////Xử lí file
-//void readListStudent(char fileName[], studentList& sl) {
-//	int n;
-//	FILE* fi = fopen(fileName, "r");
-//	if (fi == NULL) {
-//		printf("Khong the mo file!\n");
-//		exit(1);
-//	}
-//	fscanf(fi, "%d\n", &n);
-//	initSList(sl);
-//	for (int i = 0; i < n; i++)
-//	{
-//		student x;
-//		read1InfoOnFile(fi, x);
-//
-//		studentNode* p = createSNode(x);
-//		insertTail(sl, p);
-//	}
-//	fclose(fi);
-//	printf("Doc file %s thanh cong vui long nhan 2 de in\n", fileName);
-//}
-//
-//void read1InfoOnFile(FILE* fi, student& x) {
-//	fscanf(fi, "%[^|]|%[^|]|%[^|]|%d%d%d|%[^|]|%f\n", &x.maHocVien, &x.hoLot, &x.ten, &x.birthday.year, &x.birthday.month, &x.birthday.day, &x.lopHoc.maLop, &x.diem);
-//}
-////==============================
-//void input1Info(roomList room, student& x, studentList list) {
-//	fflush(stdin);
-//	do {
-//		printf("Nhap ma hoc vien: ");
-//		gets_s(x.maHocVien);
-//		if(!isValidMHV(x, list))
-//			printf("Da ton tai Ma hoc vien nay!\n");
-//	}while(!isValidMHV(x, list));
-//	
-//	fflush(stdin);
-//	
-//	printf("Nhap Ho lot : ");
-//	gets_s(x.hoLot);
-//	fflush(stdin);
-//
-//	printf("Nhap Ten: ");
-//	gets_s(x.ten);
-//	fflush(stdin);
-//
-//	do {
-//		printf("Nhap ngay thang nam sinh (yyyy/mm/dd): ");
-//		scanf("%d%d%d", &x.birthday.year, &x.birthday.month, &x.birthday.day);
-//		if (!is_valid_date(x.birthday))
-//			printf("SAI DINH DANG! Moi ban nhap lai\n");
-//
-//	} while (!is_valid_date(x.birthday));	//còn nhập sai thì nhập lại đi :)
-//	
-//	do {
-//		fflush(stdin);
-//		printf("Nhap Ma lop: ");
-//		gets_s(x.lopHoc.maLop);
-//		if (!isValidNumberclass(x, room)) //false
-//			printf("Ma lop hoc khong ton tai! Vui long nhap lai\n");
-//	} while (!isValidNumberclass(x, room)); //false
-//	
-//	fflush(stdin);
-//	printf("Nhap Diem: ");
-//	scanf("%f", &x.diem);
-//}
-//
-////==============================
-////Xoá studentNode 3 cách
-//int deleteAfter(studentList& sl, studentNode* q, student& x) {
-//	if (!q || !q->next)
-//		return 0;
-//	studentNode* p = q->next;
-//	q->next = p->next;
-//	if (sl.tailNode == p)
-//		sl.tailNode = q;
-//	x = p->value;
-//	delete p;
-//	return 1;
-//}
-//int deleteHead(studentList &sl, student &x) {
-//	if (isEmpty(sl))
-//		return 0;
-//	studentNode* p = sl.headNode;
-//	sl.headNode = sl.headNode->next;
-//	x = p->value;
-//	delete p;		
-//	return 1;
-//}
-//int deleteTail(studentList& sl, student& x) {
-//	if (isEmpty(sl))
-//		return 0;
-//	studentNode* target = sl.tailNode;	//vị trí cần xoá, đích đến.
-//	studentNode* currNode = sl.headNode;
-//	if (currNode == target)
-//		sl.headNode = sl.tailNode = NULL;
-//	else {
-//		while (currNode->next != target)
-//			currNode = currNode->next;
-//		sl.tailNode = currNode;
-//		sl.tailNode->next = NULL;
-//	}
-//		
-//	x = target->value;
-//	delete target;
-//	return 1;
-//}
-////=========================================
-////thêm studentNode 3 cách
-//int insertAfter(studentList& sl, studentNode* q, studentNode* p) {
-//	if (!p || !q)
-//		return 0;
-//	p->next = q->next;
-//	q->next = p;
-//	if (sl.tailNode = q)
-//		sl.tailNode = p;
-//	return 1;
-//}
-//
-//int insertTail(studentList& sl, studentNode* p) {
-//	if (!p)	//p==NULL
-//		return 0;
-//	if (isEmpty(sl))
-//		insertHead(sl, p);
-//	else {
-//		sl.tailNode->next = p;
-//		sl.tailNode = p;
-//	}
-//	return 1;
-//}
-//
-//int insertHead(studentList &sl, studentNode* p) {
-//	if (p == NULL)
-//		return 0;
-//	if (isEmpty(sl))
-//		sl.headNode = sl.tailNode = p;
-//	else {
-//		p->next = sl.headNode;
-//		sl.headNode = p;
-//	}
-//	return 1;
-//}
-////========================================================================
-////xuất thông tin node
-//void showSList(studentList sl) {
-//	if (isEmpty(sl)) {
-//		printf("Linked studentList is Empty\n");
-//		return;
-//	}
-//	printf("Noi dung danh sach lien ket:\n");
-//	printf("|%-s|%-14s|%-21s|%16s| %s|%-11s|%s|\n", "STT", "Ma hoc vien", "Ho lot", "Ten", "yyyy/mm/dd", "Ma lop hoc", "Diem");
-//	printf("---------------------------------------------------------------------------------------\n");
-//	studentNode* p = sl.headNode;
-//	int i = 0;
-//	while (p != NULL) {
-//		printf("  %02d", i+1);
-//		showSNode(p);
-//		i++;
-//		p = p->next;
-//	}
-//	printf("====================================================================================\n");
-//}
-//
-//void showSNode(studentNode* p) {
-//	printInfo(p->value);
-//}
-//void printInfo(student x) {
-//	printf("|%-14s|%-21s|%16s| %d/%02d/%02d|%-11s|%4.2f|\n", x.maHocVien
-//		, x.hoLot
-//		, x.ten
-//		, x.birthday.year
-//		, x.birthday.month
-//		, x.birthday.day
-//		, x.lopHoc.maLop
-//		, x.diem
-//		);
-//}
-//int isEmpty(studentList sl) {
-//	return sl.headNode == NULL;
-//}
-////init
-//roomNode* createRoomNode(classroom x) {
-//	roomNode* p = new roomNode;
-//	if (p == NULL)
-//		printf("Khong du bo nho cap phat\n");
-//	p->value = x;
-//	p->next = NULL;
-//	return p;
-//}
-//studentNode* createSNode(student x) {
-//	studentNode* p = new studentNode;
-//	if (p == NULL)
-//		printf("Khong du bo nho cap phat\n");
-//	p->value = x;
-//	p->next = NULL;
-//	return p;
-//}
-//
-//void initSList(studentList &sl) {
-//	sl.headNode = sl.tailNode = NULL;
-//}
-////PHỤ LỤC===================================================================
-////Kiểm tra
-//bool is_valid_date(Date date) {
-//	// Kiểm tra xem day, month, year có phải là số nguyên
-//	if (date.day < 1 || date.month < 1 || date.year < 1) 
-//		return false;
-//	// Kiểm tra xem year có phải là năm nhuận
-//	bool is_leap_year = (date.year % 4 == 0 && (date.year % 100 != 0 || date.year % 400 == 0));
-//
-//	// Số ngày trong mỗi tháng
-//	int days_in_month[] = { 31, 28 + is_leap_year, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
-//
-//	// Kiểm tra xem month có hợp lệ không
-//	if (date.month < 1 || date.month > 12) {
-//		return false;
-//	}
-//
-//	// Kiểm tra xem day có hợp lệ không
-//	if (date.day < 1 || date.day > days_in_month[date.month - 1]) {
-//		return false;
-//	}
-//	return true;
-//}
-////Kiểm tra lớp học đó có mở, có tồn tại
-//bool isValidNumberclass(student newStudent, roomList room) {
-//	roomNode* currNode = room.headNode;
-//	while (currNode != NULL) {
-//		if (stricmp(newStudent.lopHoc.maLop, currNode->value.maLop) == 0)
-//			return true;	//thấy rồi, mừng quá, môn này có mở lớp !
-//		currNode = currNode->next;
-//	}
-//	return false; // Ko có lớp đó để học
-//}
-////không được tồn tại học sinh trùng mã học viên
-//bool isValidMHV(student newStudent, studentList list) {
-//	studentNode* currNode = list.headNode;
-//	while (currNode != NULL) {
-//		if (stricmp(newStudent.maHocVien, currNode->value.maHocVien) == 0)
-//			return false; //tìm thấy sinh viên trùng -> NHẬP LẠI
-//		currNode = currNode->next;
-//	}
-//	return true;	//OKIE
-//}
+﻿
+
+#include "prototype-HocVien.h"
+//tim kiem
+void searchStudent(studentList sl) {
+	fflush(stdin);
+	int choice;
+	do {
+		printf("1.Tim hoc vien theo ten lop\n");
+		printf("2.Tim hoc vien theo ma hoc vien\n");
+		printf("3.Tim hoc vien theo nam sinh\n");
+		printf("0.Thoat\n");
+		printf("Chon 1..3: ");
+		scanf("%d", &choice); 
+		fflush(stdin);
+	switch (choice)
+	{
+	case 1:  {
+		char tenLop[30];
+		printf("Nhap ten lop: ");
+		gets_s(tenLop);
+		//LAMBDA EXPRESSION
+		//TÌm kiếm theo Tên lớp
+		auto searchByTenLop = [] (studentList sl, const char* nameRoom) -> void {
+			studentNode* currNode = sl.headNode;
+			bool found = false;
+			while(currNode != NULL) {
+				if(stricmp(currNode->value.lopHoc.maLop, keyTenMon(nameRoom)) == 0) {
+					found = true;	//cờ đã lên, ít nhất tìm đc 1 học viên học trong lớp 
+					showSNode(currNode);				 
+				} 
+				currNode = currNode->next;
+			}
+			if(!found) printf("Khong ton tai sinh vien nao hoc lop: %s\n", nameRoom); //lớp không có ai học
+			
+		};
+		printf("DANH SACH SINH VIEN HOC LOP: %s\n", strupr(tenLop));
+		searchByTenLop(sl, tenLop);
+		break;
+	}
+	case 2: {
+		char hocVienCoMaHocVien[16];
+		printf("Nhap ma hoc vien: ");
+		gets_s(hocVienCoMaHocVien);
+		//TÌm kiếm theo Mã học viên
+		//nếu có thì chắc chắn chỉ tồn tại 1 sv, vì mã số của mỗi người là duy nhất
+		auto searchByMaHocVien = [] (studentList sl, char maHocVien[]) -> void {
+			bool found = false;
+			studentNode* currNode = sl.headNode;
+			while (currNode != NULL) {
+				if (stricmp(currNode->value.maHocVien, maHocVien) == 0) {
+					found = true;		
+					showSNode(currNode);
+				}
+				currNode = currNode->next;	
+			}
+			if(!found) printf("Khong tim thay sinh vien co ma: %s\n", maHocVien);
+		};	
+		printf("Thong tin sinh vien co Ma hoc vien: %s\n", strupr(hocVienCoMaHocVien));
+		searchByMaHocVien(sl, hocVienCoMaHocVien);
+		break;
+	}
+	case 3: {
+		int year;
+		printf("Nhap nam sinh: ");
+		scanf("%d", &year);
+		//TÌm kiếm theo Năm sinh
+		auto searchByYOB = [](studentList sl, int year) -> void {
+			bool found = false;
+			studentNode* currNode = sl.headNode;
+			while (currNode != NULL) {
+				if (currNode->value.birthday.year == year) {
+					found = true;
+					showSNode(currNode);		
+				}
+				currNode = currNode->next;
+			}
+			if(!found) printf("Khong co sinh vien nao sinh nam: %d\n", year);
+		};		
+		printf("DANH SACH SINH VIEN SINH NAM: %d\n", year);
+		searchByYOB(sl, year);
+		break;
+	}
+	case 0:
+		break;
+	default: printf("Vui long chon 0..3!!!\n");
+	}
+}while(choice != 0);
+}
+//=================================================
+//Xử lí file
+void readListStudent(char fileName[], studentList& sl) {
+	int n;
+	FILE* fi = fopen(fileName, "r");
+	if (fi == NULL) {
+		printf("Khong the mo file!\n");
+		exit(1);
+	}
+	fscanf(fi, "%d\n", &n);
+	initSList(sl);
+	for (int i = 0; i < n; i++)
+	{
+		student x;
+		read1InfoOnFile(fi, x);
+
+		studentNode* p = createSNode(x);
+		insertTail(sl, p);
+	}
+	fclose(fi);
+	printf("Doc file %s thanh cong vui long nhan 2 de in\n", fileName);
+}
+
+void read1InfoOnFile(FILE* fi, student& x) {
+	fscanf(fi, "%[^|]|%[^|]|%[^|]|%d%d%d|%[^|]|%f\n", &x.maHocVien, &x.hoLot, &x.ten, &x.birthday.year, &x.birthday.month, &x.birthday.day, &x.lopHoc.maLop, &x.diem);
+}
+//==============================
+void input1Info(roomList room, student& x, studentList list) {
+	fflush(stdin);
+	do {
+		printf("Nhap ma hoc vien: ");
+		gets_s(x.maHocVien);
+		if(!isValidMHV(x, list))
+			printf("Da ton tai Ma hoc vien nay!\n");
+	}while(!isValidMHV(x, list));
+	
+	fflush(stdin);
+	printf("Nhap Ho lot : ");
+	gets_s(x.hoLot);
+	
+	fflush(stdin);
+	printf("Nhap Ten: ");
+	gets_s(x.ten);
+	fflush(stdin);
+	do {
+		printf("Nhap ngay thang nam sinh (yyyy/mm/dd): ");
+		scanf("%d%d%d", &x.birthday.year, &x.birthday.month, &x.birthday.day);
+		if (!is_valid_date(x.birthday))
+			printf("SAI DINH DANG! Moi ban nhap lai\n");
+	} while (!is_valid_date(x.birthday));	//còn nhập sai thì nhập lại đi :)
+	
+	do {
+		fflush(stdin);
+		printf("Nhap Ma lop: ");
+		gets_s(x.lopHoc.maLop);
+		if (!isValidNumberclass(x, room)) //false
+			printf("Ma lop hoc khong ton tai! Vui long nhap lai\n");
+	} while (!isValidNumberclass(x, room)); //false
+	
+	fflush(stdin);
+	printf("Nhap Diem: ");
+	scanf("%f", &x.diem);
+}
+//==============================
+//Xoá studentNode 3 cách
+int deleteAfter(studentList& sl, studentNode* q, student& x) {
+	if (!q || !q->next)
+		return 0;
+	studentNode* p = q->next;
+	q->next = p->next;
+	if (sl.tailNode == p)
+		sl.tailNode = q;
+	x = p->value;
+	delete p;
+	return 1;
+}
+int deleteHead(studentList &sl, student &x) {
+	if (isEmpty(sl))
+		return 0;
+	studentNode* p = sl.headNode;
+	sl.headNode = sl.headNode->next;
+	x = p->value;
+	delete p;		
+	return 1;
+}
+int deleteTail(studentList& sl, student& x) {
+	if (isEmpty(sl))
+		return 0;
+	studentNode* target = sl.tailNode;	//vị trí cần xoá, đích đến.
+	studentNode* currNode = sl.headNode;
+	if (currNode == target)
+		sl.headNode = sl.tailNode = NULL;
+	else {
+		while (currNode->next != target)
+			currNode = currNode->next;
+		sl.tailNode = currNode;
+		sl.tailNode->next = NULL;
+	}
+		
+	x = target->value;
+	delete target;
+	return 1;
+}
+//=========================================
+//thêm studentNode 3 cách
+int insertAfter(studentList& sl, studentNode* q, studentNode* p) {
+	if (!p || !q)
+		return 0;
+	p->next = q->next;
+	q->next = p;
+	if (sl.tailNode = q)
+		sl.tailNode = p;
+	return 1;
+}
+
+int insertTail(studentList& sl, studentNode* p) {
+	if (!p)	//p==NULL
+		return 0;
+	if (isEmpty(sl))
+		insertHead(sl, p);
+	else {
+		sl.tailNode->next = p;
+		sl.tailNode = p;
+	}
+	return 1;
+}
+
+int insertHead(studentList &sl, studentNode* p) {
+	if (p == NULL)
+		return 0;
+	if (isEmpty(sl))
+		sl.headNode = sl.tailNode = p;
+	else {
+		p->next = sl.headNode;
+		sl.headNode = p;
+	}
+	return 1;
+}
+//========================================================================
+//xuất thông tin node
+void showSList(studentList sl) {
+	if (isEmpty(sl)) {
+		printf("Linked studentList is Empty\n");
+		return;
+	}
+	printf("Noi dung danh sach lien ket:\n");
+	printf("|%-s|%-14s|%-21s|%16s| %s|%-11s|%s|\n", "STT", "Ma hoc vien", "Ho lot", "Ten", "yyyy/mm/dd", "Ma lop hoc", "Diem");
+	printf("---------------------------------------------------------------------------------------\n");
+	studentNode* p = sl.headNode;
+	int i = 0;
+	while (p != NULL) {
+		printf("  %02d", i+1);
+		showSNode(p);
+		i++;
+		p = p->next;
+	}
+	printf("====================================================================================\n");
+}
+
+void showSNode(studentNode* p) {
+	printInfo(p->value);
+}
+void printInfo(student x) {
+	printf("|%-14s|%-21s|%16s| %d/%02d/%02d|%-11s|%4.2f|\n", x.maHocVien
+		, x.hoLot
+		, x.ten
+		, x.birthday.year
+		, x.birthday.month
+		, x.birthday.day
+		, x.lopHoc.maLop
+		, x.diem
+		);
+}
+int isEmpty(studentList sl) {
+	return sl.headNode == NULL;
+}
+//init
+roomNode* createRoomNode(classroom x) {
+	roomNode* p = new roomNode;
+	if (p == NULL)
+		printf("Khong du bo nho cap phat\n");
+	p->value = x;
+	p->next = NULL;
+	return p;
+}
+studentNode* createSNode(student x) {
+	studentNode* p = new studentNode;
+	if (p == NULL)
+		printf("Khong du bo nho cap phat\n");
+	p->value = x;
+	p->next = NULL;
+	return p;
+}
+
+void initSList(studentList &sl) {
+	sl.headNode = sl.tailNode = NULL;
+}
+//PHỤ LỤC===================================================================
+//Kiểm tra
+bool is_valid_date(Date date) {
+	// Kiểm tra xem day, month, year có phải là số nguyên
+	if (date.day < 1 || date.month < 1 || date.year < 1) 
+		return false;
+	// Kiểm tra xem year có phải là năm nhuận
+	bool is_leap_year = (date.year % 4 == 0 && (date.year % 100 != 0 || date.year % 400 == 0));
+
+	// Số ngày trong mỗi tháng
+	int days_in_month[] = { 31, 28 + is_leap_year, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+	// Kiểm tra xem month có hợp lệ không
+	if (date.month < 1 || date.month > 12) {
+		return false;
+	}
+
+	// Kiểm tra xem day có hợp lệ không
+	if (date.day < 1 || date.day > days_in_month[date.month - 1]) {
+		return false;
+	}
+	return true;
+}
+//Kiểm tra lớp học đó có mở, có tồn tại
+bool isValidNumberclass(student newStudent, roomList room) {
+	roomNode* currNode = room.headNode;
+	while (currNode != NULL) {
+		if (stricmp(newStudent.lopHoc.maLop, currNode->value.maLop) == 0)
+			return true;	//thấy rồi, mừng quá, môn này có mở lớp !
+		currNode = currNode->next;
+	}
+	return false; // Ko có lớp đó để học
+}
+//không được tồn tại học sinh trùng mã học viên
+bool isValidMHV(student newStudent, studentList list) {
+	studentNode* currNode = list.headNode;
+	while (currNode != NULL) {
+		if (stricmp(newStudent.maHocVien, currNode->value.maHocVien) == 0)
+			return false; //tìm thấy sinh viên trùng -> NHẬP LẠI
+		currNode = currNode->next;
+	}
+	return true;	//OKIE
+}
--- QUANLIHOCVIEN/nhom2-CungCode/quanLiHocVien_main.cpp
+++ QUANLIHOCVIEN/nhom2-CungCode/quanLiHocVien_main.cpp
@@ -1,68 +1,130 @@
-﻿//#include "prototype-HocVien.h"
-//void process();
-//void printMenu();
-//
-//int main() {
-//	process();
-//	getch();
-//	return 0;
-//}
-//void process() {
-//	student x;
-//	studentList sl;
-//	roomList room;
-//	studentNode* p;
-//	int choice;
-//
-//	do {
-//		printMenu();
-//		printf("Choose 0..9: ");
-//		scanf("%d", &choice);
-//		switch (choice)
-//		{
-//		case 1:
-//			readListStudent("hocVien.txt", sl);
-//			readListRoom("lopHoc.txt", room);	
-//			break;
-//		case 2:
-//			showSList(sl);
-//			showRoomList(room);
-//			break;
-//		case 3:
-//			input1Info(room, x, sl);
-//			p = createSNode(x);
-//			insertTail(sl, p);
-//			break;
-//		case 4:
-//			searchStudent(sl);
-//			break;
-//		default:
-//			printf("Vui long nhap tu 0..9\n");
-//			break;
-//		}
-//	} while (choice != 0);
-//}
-//
-//void printMenu() {
-//	printf("=================================================================\n");
-//printf("|                                                               |\n");
-//printf("|            Chao mung ban den voi chuong trinh                 |\n");
-//printf("|                 QUAN LI HOC VIEN                              |\n");
-//printf("|                                                               |\n");
-//printf("|---------------------------------------------------------------|\n");
-//printf("| 1.Doc thong tin lop hoc va hoc vien tu FILE                   |\n");
-//printf("| 2.Xuat thong tin cac lop hoc va sinh vien hien tai            |\n");
-//printf("| 3.Them 1 hoc vien vao danh sach - bang ban phim               |\n");
-//printf("| 4.Tim kiem hoc vien                                           |\n");
-//printf("| 5.In thong tin lop hoc                                        |\n");
-//printf("| 6.Tim hoc vien co diem cao nhat                               |\n");
-//printf("| 7.Tim hoc vien hoc o lop co hoc phi cao nhat                  |\n");
-//printf("| 8.Sap xep hoc vien theo ma lop, hoc vien cung lop thi sap xep |\n");
-//printf("|   Diem giam dan                                               |\n");
-//printf("| 9.Thong tin hoc vien co diem chua dat (<5), sap xep giam dan  |\n");
-//printf("|   theo ten                                                    |\n");
-//printf("| 10.In danh sach sinh vien dat HOC BONG (Diem >=8)             |\n");
-//printf("=================================================================\n");
-//
-//
-//}
+﻿#include "prototype-HocVien.h"
+void process();
+void printMenu();
+int main() {
+    process();
+    getch();
+    return 0;
+}
+
+void process() {
+    student x;
+    studentList sl;
+    roomList room;
+    studentNode* p;
+    int choice;
+    bool isLoaded = false;  // Biến cờ để kiểm tra xem danh sách đã được tải lên hay chưa
+
+    do {
+        printMenu();
+        printf("Choose 0..10: ");
+        scanf("%d", &choice);
+        switch (choice)
+        {
+        case 1:
+            readListStudent("hocVien.txt", sl);
+            readListRoom("lopHoc.txt", room);
+            isLoaded = true;  // Đặt cờ là true khi danh sách đã được tải
+            break;
+        case 2:
+            if (isLoaded) {
+                showSList(sl);
+                showRoomList(room);
+            } 
+			else 
+                printf("Danh sach chua duoc tai. Vui long tai danh sach truoc.\n");
+            break;
+        case 3:
+            if (isLoaded) {
+                input1Info(room, x, sl);
+                p = createSNode(x);
+                insertTail(sl, p);
+            } 
+			else 
+                printf("Danh sach chua duoc tai. Vui long tai danh sach truoc.\n");
+            break;
+        case 4:
+            if (isLoaded) {
+                searchStudent(sl);
+            }
+			else 
+                printf("Danh sach chua duoc tai. Vui long tai danh sach truoc.\n");
+            break;
+        // Các case khác cũng thêm kiểm tra isLoaded tương tự như trên
+        case 5:
+            if (isLoaded) {
+                
+            } 
+			else 
+                printf("Danh sach chua duoc tai. Vui long tai danh sach truoc.\n");
+            break;
+        case 6:
+            if (isLoaded) {
+               
+            } 
+			else 
+                printf("Danh sach chua duoc tai. Vui long tai danh sach truoc.\n");
+            
+            break;
+        case 7:
+            if (isLoaded) {
+                
+            } 
+			else 
+                printf("Danh sach chua duoc tai. Vui long tai danh sach truoc.\n");
+            break;
+        case 8:
+            if (isLoaded) {
+                // Thực hiện các chức năng liên quan đến case 8
+            } else 
+                printf("Danh sach chua duoc tai. Vui long tai danh sach truoc.\n");
+            break;
+        case 9:
+            if (isLoaded) {
+                // Thực hiện các chức năng liên quan đến case 9
+            } 
+			else
+                printf("Danh sach chua duoc tai. Vui long tai danh sach truoc.\n");
+            break;
+        case 10:
+            if (isLoaded) {
+                // Thực hiện các chức năng liên quan đến case 10
+            } 
+			else
+                printf("Danh sach chua duoc tai. Vui long tai danh sach truoc.\n");
+            break;
+        case 0:
+            break;
+        default:
+            printf("Vui long nhap tu 0..10\n");
+            break;
+        }
+    } while (choice != 0);
+    printf("Bye! Have a nice day\n");
+}
+
+void printMenu() {
+    printf("=================================================================\n");
+    printf("|                                                               |\n");
+    printf("|            Chao mung ban den voi chuong trinh                 |\n");
+    printf("|                 QUAN LI HOC VIEN                              |\n");
+    printf("|                                                               |\n");
+    printf("|---------------------------------------------------------------|\n");
+    printf("| 1.Doc thong tin lop hoc va hoc vien tu FILE                   |\n");
+    printf("| 2.Xuat thong tin cac lop hoc va sinh vien hien tai            |\n");
+    printf("| 3.Them 1 hoc vien vao danh sach - bang ban phim               |\n");
+    printf("| 4.Tim kiem hoc vien                                           |\n");
+    printf("| 5.In thong tin lop hoc                                        |\n");
+    printf("| 6.Tim hoc vien co diem cao nhat                               |\n");
+    printf("| 7.Tim hoc vien hoc o lop co hoc phi cao nhat                  |\n");
+    printf("| 8.Sap xep hoc vien theo ma lop, hoc vien cung lop thi sap xep |\n");
+    printf("|   Diem giam dan                                               |\n");
+    printf("| 9.Thong tin hoc vien co diem chua dat (<5), sap xep giam dan  |\n");
+    printf("|   theo ten                                                    |\n");
+    printf("| 10.In danh sach sinh vien dat HOC BONG (Diem >=8)             |\n");
+    printf("=================================================================\n");
+}
+
+//============================================================================
+//PHỤ LỤC
+//TH: Người dùng chưa load danh sách lên thì khôngn đc sử dụnngn các tính năng
